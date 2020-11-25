fd3611ae2e8e6b44622f306589114c9c1db35668
Christophe Morisset <chris.morisset@gmail.com>
2020-11-25 17:55:00 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='OxygenMLP',
     version='0.2.1',
     packages=['OxygenMLP'],
-    package_data = {'OxygenMLP': 'bestModels.pkl'},
+    package_data = {'OxygenMLP': ['bestModels.pkl']},
 #    license='MIT',
     description='neural network, multi-layer perceptron model that can predict oxygen abundance using strong emission lines',
     long_description=open('README.md').read(),
